b0ac72ef1856f80ac2cf9a8c2258cd96f067a839
Bashar Mengana <mengana@kth.se>
2019-10-06 22:11:36 +0200

    added test classes and repository mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Graph from "./Components/Graph/Graph"
+
+import {getDataSet1, getDataSet2, getDataSet3} from './DateRepository/DataRepository'
+
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Graph></Graph>
     </div>
   );
 }
---
+++ src/Components/Graph/Graph.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const Graph: React.FC<Props> = () => {
+  return (
+    <div>
+      test
+    </div>
+  )
+}
+
+export default Graph;
