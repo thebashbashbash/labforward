ffe4babb214cf5222be3964f1c9823601aec676e
Bashar Mengana <mengana@kth.se>
2019-10-07 20:56:12 +0200

    finished graph

--- src/Components/Graph/Graph.tsx
+++ src/Components/Graph/Graph.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { VictoryArea, VictoryChart, VictoryLabel } from 'victory';
+import { VictoryArea, VictoryChart, VictoryAxis, VictoryZoomContainer } from 'victory';
 
 import { Consumer } from '../../StateManagement/StateManagement';
 
@@ -8,11 +8,33 @@ interface Props {
   
 }
 
+
 const Graph = () => {
   return (
     <Consumer>
         {appContext => (
-        <VictoryChart>
+        <VictoryChart
+        domain={{y: [1, 15]}}
+        containerComponent={<VictoryZoomContainer zoomDomain={{x: [0, 50], y: [0, Math.max(...appContext.state.data)]}}/>}
+        >
+            <VictoryAxis
+              crossAxis
+              tickValues = {appContext.state.data.map((_, index) => index + 1)}
+              style={{
+                grid: {stroke: "rgba(68, 71, 90, 0.7)"},
+                tickLabels: {fontSize: 8, padding: 3}
+              }}
+              fixLabelOverlap
+            />
+
+            <VictoryAxis
+              dependentAxis
+              style={{
+                grid: {stroke: "rgba(40, 52, 54, 0.7)"},
+                tickLabels: {fontSize: 8, padding: 3}
+              }}
+              fixLabelOverlap
+            />
           <VictoryArea
             style={{ data: { fill: "rgba(98, 114, 164,1)" } }}
             data={appContext.state.data}
